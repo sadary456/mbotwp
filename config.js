58ee97486bcada4493b55d4508a7508c01ae9b0a
sadary456 <101031532+sadary456@users.noreply.github.com>
2025-01-18 19:55:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-   SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=vU0z1LYI#fNlCGos-pk5-goUMomchasRRmjsSuZhty0yB9QqFns0' : process.env.SESSION_ID, 
+   SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=3JlG2brB#xL7lkr0N1F-bIog9L28OIXz_p-CiHoEt95pqxG1UZKU' : process.env.SESSION_ID, 
    DATABASE_URL: process.env.DATABASE_URL === undefined ? 'postgres://movie_my_user:BDXztL7cmv1gV26b9eCsAseSMp7tqyvW@dpg-co1n7jvsc6pc73ctrku0-a.oregon-postgres.render.com/movie_my' : process.env.DATABASE_URL,
    PREFIX: process.env.PREFIX || '.' ,
    JID: process.env.JID || `94719199757@s.whatsapp.net` , 
